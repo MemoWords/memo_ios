41709d5591ddd60282c579c9793daaf4ff1df382
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2020-08-23 19:30:07 -0300

    Some Unit Tests Done

---
+++ memoTests/CardTests.swift
@@ -0,0 +1,25 @@
+//
+//  CardTests.swift
+//  memoTests
+//
+//  Created by Elias Ferreira on 23/08/20.
+//  Copyright © 2020 Academy IFCE. All rights reserved.
+//
+
+import XCTest
+
+@testable import memo
+
+class CardTests: XCTestCase {
+    func test_card_init_returnsACardWhitDefaultValues() {
+        let sut = Card(content: "test")
+        
+        let output1 = sut.content
+        let output2 = sut.nextStudyDay
+        let output3 = sut.lastDaysIncremented
+        
+        XCTAssertEqual(output1, "test")
+        XCTAssertEqual(output2, "23-08-2020")
+        XCTAssertEqual(output3, 1)
+    }
+}
