747c48c95fd3dd6b7955aee0f60c67cabeaad15d
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2020-10-24 14:25:58 -0300

    Colors

---
+++ memo/Extensions/Color+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Extension.swift
+//  memo
+//
+//  Created by Elias Ferreira on 24/10/20.
+//  Copyright © 2020 Academy IFCE. All rights reserved.
+//
+import UIKit
+
+extension UIColor {
+    static let memoBlack: UIColor = UIColor(named: "memoBlack")!
+    static let memoWhite: UIColor = UIColor(named: "memoWhite")!
+    static let memoBlue: UIColor = UIColor(named: "memoBlue")!
+    static let memoDarkBlue: UIColor = UIColor(named: "memoDarkBlue")!
+    static let memoLightBlue: UIColor = UIColor(named: "memoLightBlue")!
+    static let memoSecondBlue: UIColor = UIColor(named: "memoSecondBlue")!
+    static let memoRed: UIColor = UIColor(named: "memoRed")!
+    static let memoDarkGray: UIColor = UIColor(named: "memoDarkGray")!
+    static let memoLightGray: UIColor = UIColor(named: "memoLightGray")!
+}
