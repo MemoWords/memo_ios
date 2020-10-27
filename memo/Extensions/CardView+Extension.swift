90c64b1c34a548a7580a3a5bbf0672442dd6acfc
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2020-10-26 21:05:20 -0300

    Review View Finished

---
+++ memo/Extensions/CardView+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  CardView+Extension.swift
+//  memo
+//
+//  Created by Elias Ferreira on 26/10/20.
+//  Copyright © 2020 Academy IFCE. All rights reserved.
+//
+
+import Foundation
---
+++ memo/View/Components/CardTableView.swift
@@ -0,0 +1,21 @@
+//
+//  CardTableView.swift
+//  memo
+//
+//  Created by Elias Ferreira on 26/10/20.
+//  Copyright © 2020 Academy IFCE. All rights reserved.
+//
+
+import UIKit
+
+class CardTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
