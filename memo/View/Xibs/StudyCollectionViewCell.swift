2dbbcc08f628b902557c6413ac93c22bf2381f28
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2021-02-09 10:05:51 -0300

    cell_created

---
+++ memo/View/Xibs/StudyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  StudyCollectionViewCell.swift
+//  memo
+//
+//  Created by Elias Ferreira on 09/02/21.
+//  Copyright © 2021 Academy IFCE. All rights reserved.
+//
+
+import UIKit
+
+class StudyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
