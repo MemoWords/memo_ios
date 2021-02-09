ee01d7286f64b4511c42eed0c9d551b352484161
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2021-02-09 10:46:16 -0300

    folder_cell

---
+++ memo/View/Xibs/FolderTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FolderTableViewCell.swift
+//  memo
+//
+//  Created by Elias Ferreira on 09/02/21.
+//  Copyright © 2021 Academy IFCE. All rights reserved.
+//
+
+import UIKit
+
+class FolderTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var mainFrame: UIView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var bodyLabel: UILabel!
+
+  override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
