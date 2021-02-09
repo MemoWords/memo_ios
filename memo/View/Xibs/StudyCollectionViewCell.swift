b8b9d27df8aed6e0e620ae4affdd247f6754696c
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2021-02-09 10:16:52 -0300

    studycell_controller

--- memo/View/Xibs/StudyCollectionViewCell.swift
+++ memo/View/Xibs/StudyCollectionViewCell.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class StudyCollectionViewCell: UICollectionViewCell {
 
+    // Items
+    @IBOutlet weak var mainFrame: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // Style.
+        mainFrame.layer.cornerRadius = 20
+        mainFrame.layer.cornerRadius = 20
+        mainFrame.layer.shadowColor = UIColor.black.cgColor
+        mainFrame.layer.shadowOpacity = 0.35
+        mainFrame.layer.shadowRadius = 4
+        mainFrame.layer.shadowOffset = .init(width: 0, height: 3)
+    }
+
+    // Prepare the cell for reuse at the table.
+    override func prepareForReuse() {
+        titleLabel.text = nil
+        bodyLabel.text = nil
     }
 
 }
