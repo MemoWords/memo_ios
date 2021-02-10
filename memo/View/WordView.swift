6a42b0edb57af1716774c81f023702b7993fe135
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2021-02-09 22:35:47 -0300

    word cell created

---
+++ memo/View/WordView.swift
@@ -0,0 +1,63 @@
+//
+//  WordView.swift
+//  memo
+//
+//  Created by Elias Ferreira on 09/02/21.
+//  Copyright © 2021 Academy IFCE. All rights reserved.
+//
+
+import UIKit
+
+class WordView: UIView {
+
+    lazy var totalLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "SF Pro Text Medium", size: 16)
+        label.textColor = .bodyColor
+        return label
+    }()
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.backgroundColor = .clear
+        tableView.alwaysBounceVertical = true
+        tableView.separatorStyle = .none
+        tableView.allowsSelection = false
+        tableView.rowHeight = 76
+        return tableView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .backgroundColor
+        addSubviews(totalLabel, tableView)
+        setUpLabel()
+        setUpTableView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+extension WordView {
+    func setUpLabel() {
+        totalLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            totalLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 30),
+            totalLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 22),
+            totalLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -22)
+        ])
+    }
+
+    func setUpTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: totalLabel.bottomAnchor, constant: 10),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)
+        ])
+    }
+}
---
+++ memo/View/Xibs/WordTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  WordTableViewCell.swift
+//  memo
+//
+//  Created by Elias Ferreira on 09/02/21.
+//  Copyright © 2021 Academy IFCE. All rights reserved.
+//
+
+import UIKit
+
+class WordTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var mainFrame: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        mainFrame.layer.cornerRadius = 16
+        mainFrame.layer.shadowColor = UIColor.black.cgColor
+        mainFrame.layer.shadowOpacity = 0.03
+        mainFrame.layer.shadowRadius = 3
+        mainFrame.layer.shadowOffset = .init(width: 1, height: 2)
+    }
+    
+}
