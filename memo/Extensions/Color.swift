bc6de0e8c6bc4b7a06fac35e2e09904297bb6089
Elias Ferreira <eliasferreira@MacBook-Pro-de-Elias.local>
2021-03-11 22:20:41 -0300

    Color extention

--- memo/Extensions/Color.swift
+++ memo/Extensions/Color.swift
@@ -8,17 +8,10 @@
 import UIKit
 
 extension UIColor {
-    // OLD
-    static let memoBlack: UIColor = UIColor(named: "memoBlack")!
-    static let memoWhite: UIColor = UIColor(named: "memoWhite")!
-    static let memoBlue: UIColor = UIColor(named: "memoBlue")!
     static let memoDarkBlue: UIColor = UIColor(named: "memoDarkBlue")!
     static let memoLightBlue: UIColor = UIColor(named: "memoLightBlue")!
     static let memoSecondBlue: UIColor = UIColor(named: "memoSecondBlue")!
-    static let memoRed: UIColor = UIColor(named: "memoRed")!
     static let memoDarkGray: UIColor = UIColor(named: "memoDarkGray")!
-    static let memoLightGray: UIColor = UIColor(named: "memoLightGray")!
-    // NEW
     static let primaryColor: UIColor = UIColor(named: "primary_color")!
     static let secondaryColor: UIColor = UIColor(named: "secondary_color")!
     static let inactiveColor: UIColor = UIColor(named: "inactive_color")!
@@ -27,4 +20,22 @@ extension UIColor {
     static let bodyColor: UIColor = UIColor(named: "body_color")!
     static let backgroundColor: UIColor = UIColor(named: "background_color")!
     static let whiteColor: UIColor = UIColor(named: "white_color")!
+
+// MARK: -
+
+    // Basic
+    static let memoBlue            = UIColor(named: "memo_blue")
+    static let memoRed             = UIColor(named: "memo_red")
+    static let memoGreen           = UIColor(named: "memo_green")
+    static let memoYellow          = UIColor(named: "memo_yellow")
+    // Uncolored
+    static let memoBlack           = UIColor(named: "memo_black")
+    static let memoGray            = UIColor(named: "memo_gray")
+    static let memoLightGray       = UIColor(named: "memo_lightGray")
+    static let memoWhite           = UIColor(named: "memo_white")
+    // DarkMode
+    static let memoBackground      = UIColor(named: "memo_background")
+    static let memoLightBackground = UIColor(named: "memo_lightBackground")
+    static let memoText            = UIColor(named: "memo_text")
+    static let memoTextBackground  = UIColor(named: "memo_textBackground")
 }
